a214a530037d88454155ee8b770d6e30c2ca368c
Anthony DeVenuto <adevenuto@macbook-m1.local>
2024-01-01 18:59:09 -0600

    added basic layout components Sidebar/Navbar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react'
+import NavBar from '@/components/NavBar'
+import { SideBar } from '@/components/SideBar'
+
+export const layout = ({
+  children
+}:{
+  children: ReactNode
+}) => {
+  return (
+    <div className="relative h-full">
+      <div className="z-50 hidden h-full bg-gray-900 md:flex md:fixed md:flex-col md:inset-y-0 md:w-60">
+        <SideBar />
+      </div>
+      <main className='md:pl-60'>
+        <NavBar />
+        { children }
+      </main>
+    </div>
+  )
+}
+
+export default layout
---
+++ components/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SideBar = () => {
+  return (
+    <div className="flex flex-col text-white">
+      Sidebar
+    </div>
+  )
+}
